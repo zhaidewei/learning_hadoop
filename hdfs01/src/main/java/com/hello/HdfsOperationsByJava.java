2b986892f123cef361b673c50bd42ae7be7aac8c
dzhai <dewei.zhai.osv@fedex.com>
2020-05-30 17:31:57 +0200

    runnable version

--- hdfs01/src/main/java/com/hello/HdfsOperationsByJava.java
+++ hdfs01/src/main/java/com/hello/HdfsOperationsByJava.java
@@ -1,38 +1,34 @@
+package com.hello;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Hdfs;
 import org.apache.hadoop.fs.Path;
 
-import java.io.File;
 import java.io.IOException;
 
 public class HdfsOperationsByJava {
 
     public static void main(String[] args) throws IOException {
        mkDir();
-       copyFromLocal();
+       copyFromLocal(args);
     }
 
-
     static void mkDir() throws IOException {
-        String nameNodeURL =  "hdfs://node01:8020";
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", nameNodeURL);
+        configuration.set("fs.defaultFS", "hdfs://node01:8020");
         FileSystem fs = FileSystem.get(configuration);
         fs.mkdirs(new Path("/kkb/dir1"));
         fs.close();
     }
 
-    static void copyFromLocal() throws IOException {
-        String nameNodeURL =  "hdfs://node01:8020";
+    static void copyFromLocal(String[] args) throws IOException {
+
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", nameNodeURL);
+        configuration.set("fs.defaultFS", "hdfs://node01:8020");
         FileSystem fs = FileSystem.get(configuration);
-        String currentDir = new File("").getAbsolutePath();
         fs.copyFromLocalFile(
-                new Path("file://" + currentDir + "/pom.xml"),
-                new Path( nameNodeURL + "/kkb/dir1/pom.xml"));
+                new Path(args[0]),
+                new Path(args[1]));
         fs.close();
     }
 }
