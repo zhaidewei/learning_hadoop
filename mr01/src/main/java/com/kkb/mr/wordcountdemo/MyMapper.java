0a9d5a596c29995afdf80574aa9117174b7505ba
dzhai <dewei.zhai.osv@fedex.com>
2020-06-01 14:31:54 +0200

    bugfix

--- mr01/src/main/java/com/kkb/mr/wordcountdemo/MyMapper.java
+++ mr01/src/main/java/com/kkb/mr/wordcountdemo/MyMapper.java
@@ -23,7 +23,7 @@ public class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = key.toString();
+        String line = value.toString();
         String[] words = line.split(",");
 
         Text outKey = new Text();
